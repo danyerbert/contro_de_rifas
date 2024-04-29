aed5c90bf74f1b9ce6d8457ee4dcd39587f84fe4
danyerbert <danyerbert@gmail.com>
2024-04-29 19:34:14 -0400

    Correcion de validacion de rifa millonaria

--- php/registroMillonario.php
+++ php/registroMillonario.php
@@ -60,34 +60,34 @@ if ($_POST) {
     // Va
     $valor = 2;
 
-    $sqlNumeroBloqueado = "SELECT numero, fecha FROM numero_bloqueado WHERE numero = '$numeroOne' AND fecha = '$fecha' AND tipo_de_rifa = '$tipo_de_rifa'";
+    $sqlNumeroBloqueado = "SELECT numero FROM numero_bloqueado WHERE fecha = '$fecha' AND tipo_de_rifa = '$tipo_de_rifa'";
     $resultadoBloqueado = $mysqli->query($sqlNumeroBloqueado);
     $rowBloqueado = mysqli_fetch_assoc($resultadoBloqueado);
     $NumeroBloqueado = $rowBloqueado['numero'];
 
     if ($numeroOne == $NumeroBloqueado) {
         $valido['success'] = false;
-        $valido['mensaje'] = "Numero no permitido.";
+        $valido['mensaje'] = "Numero no permitido: " . $numeroOne;
     }elseif (
         $numeroDos == $NumeroBloqueado
     ) {
         $valido['success'] = false;
-        $valido['mensaje'] = "Numero no permitido.";
+        $valido['mensaje'] = "Numero no permitido: " . $numeroDos;
     }elseif (
         $numeroTres == $NumeroBloqueado
     ) {
         $valido['success'] = false;
-        $valido['mensaje'] = "Numero no permitido.";
+        $valido['mensaje'] = "Numero no permitido: " . $numeroTres;
     }elseif (
         $numeroCuatro == $NumeroBloqueado
     ) {
         $valido['success'] = false;
-        $valido['mensaje'] = "Numero no permitido.";
+        $valido['mensaje'] = "Numero no permitido: " . $numeroCuatro;
     }elseif (
         $numeroCinco == $NumeroBloqueado
     ) {
         $valido['success'] = false;
-        $valido['mensaje'] = "Numero no permitido.";
+        $valido['mensaje'] = "Numero no permitido: " . $numeroCinco;
     }else {
         $sql = "INSERT INTO registro_numero_millonaria (id_millonaria, numero_one, numero_dos, numero_tres, numero_cuatro, numero_cinco, vendedor, valor, fecha, nombre, cedula) VALUES (NULL, '$numeroOne','$numeroDos','$numeroTres','$numeroCuatro','$numeroCinco','$vendedor','$valor','$fecha','$nombre','$cedula')";
         $resultadoRegistro = $mysqli->query($sql);
